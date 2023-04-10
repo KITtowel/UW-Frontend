5691a2fb3e0a90b6804a1527427947f27fde555b
suhye0n <contact.suhyeon@gmail.com>
2023-04-10 11:22:04 +0900

    메인페이지 삭제

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Main from "./pages/Main";
 import Signup from "./pages/Signup";
 import Mypage from "./pages/Mypage";
 import { StoreMap } from "./pages";
@@ -11,11 +10,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<StoreMap />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/storeMap" element={<StoreMap />} />
       </Routes>
     </BrowserRouter>
   );
