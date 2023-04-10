7b8d50e5de21154eab34c2e2a153e402b3e0b7e7
suhye0n <contact.suhyeon@gmail.com>
2023-04-11 01:28:20 +0900

    내 정보 수정

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,162 +1,310 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Logo from "../assets/logo.png";
-import Button from "../components/Button";
 import User from "../components/User";
 import LogoBtn from "../components/LogoBtn";
 import { Link } from "react-router-dom";
+import Logo2 from "../assets/logo2.png";
+import Profile from "../assets/profile.png";
 
-const Ocean = styled.div`
-  background: #3b21ff;
-  height: 10%;
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  z-index: -1;
+const cityOptions = {
+  경상북도: [
+    "경산시",
+    "경주시",
+    "고령군",
+    "구미시",
+    "군위군",
+    "김천시",
+    "문경시",
+    "봉화군",
+    "상주시",
+    "성주군",
+    "안동시",
+    "영덕군",
+    "영양군",
+    "영주시",
+    "영천시",
+    "예천군",
+    "울릉군",
+    "울진군",
+    "의성군",
+    "청도군",
+    "청송군",
+    "칠곡군",
+    "포항시 남구",
+    "포항시 북구",
+  ],
+  대구광역시: ["남구", "동구", "북구", "서구", "수성구", "달서구", "달성군"],
+};
+
+const Header = styled.div`
+  position: fixed;
+  top: 0;
+  left: 10%;
+  width: 80%;
+  height: 80px;
+  background: #fff;
+  border-bottom: 1px solid #969696;
+  display: flex;
+  align-items: center;
 `;
 
-const Wave = styled.div`
-  background: url(https://venkat369.github.io/development/wave.svg) repeat-x; 
-  position: absolute;
-  top: -198px;
-  width: 6400px;
-  height: 198px;
-  animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
+const NavButton = styled.button`
+  background: transparent;
+  color: ${props => (props.selected ? "#24A1E8" : "#000000")};
+  border: none;
+  text-decoration: ${props => (props.selected ? "underline" : "none")};
+  cursor: pointer;
+  font-size: 1rem;
+  margin: 30px 1rem;
+  vertical-align: top;
+`;
 
-  &:nth-of-type(2) {
-    top: -175px;
-    animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
-    opacity: 1;
+const EditButton = styled.button`
+  display: inline-block;
+  margin: 5px;
+  padding: 6px 30px;
+  color: #636363;
+  font-weight: 600;
+  text-transform: uppercase;
+  background: #fff;
+  border: 1px solid;
+  border-radius: 5px;
+  outline: 0;
+  cursor: pointer;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-out;
+  :hover {
+    background: #636363;
+    color: #fff;
   }
-}
+`;
 
-@keyframes wave {
-  0% {
-    margin-left: 0;
+const DeleteButton = styled.button`
+  display: inline-block;
+  margin: 5px;
+  padding: 6px 30px;
+  color: #ff0000;
+  font-weight: 600;
+  text-transform: uppercase;
+  background: #fff;
+  border: 1px solid;
+  border-radius: 5px;
+  outline: 0;
+  cursor: pointer;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-out;
+  :hover {
+    background: #ff0000;
+    color: #fff;
   }
-  100% {
-    margin-left: -1600px;
-  }
-}
-
-@keyframes swell {
-  0%, 100% {
-    transform: translate3d(0, -25px, 0);
-  }
-  50% {
-    transform: translate3d(0, 5px, 0);
-  }
-}
 `;
 
 const Container = styled.div`
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 28rem;
-  height: 70vh;
-  overflow: auto;
-  justify-content: center;
-  align-items: center;
-  margin: 6rem auto 0 auto;
-  padding: 2rem 2.5rem;
-  border: none;
-  outline: none;
-  border-radius: 0.25rem;
-  color: #121212;
+  text-align: left;
+  width: 80%;
+  margin: 80px auto 0;
+  border-bottom: 1px solid #969696;
   background: #ffffff;
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-    0 4px 6px -2px rgba(0, 0, 0, 0.05);
 `;
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  margin: 15px 0;
+const LogoIcon = styled.img`
+  width: 50px;
+  margin: 15px 50px;
 `;
 
 const ProfilePicture = styled.img`
-  width: 200px;
-  height: 200px;
+  margin: 10px 55px;
+  width: 80px;
+  height: 80px;
   border-radius: 50%;
   object-fit: cover;
 `;
 
-const InfoContainer = styled.div``;
-
-const InfoRow = styled.div`
-  margin: 10px;
-  padding: 10px;
-  border-bottom: 1px solid gray;
+const Select = styled.select`
+  float: left;
+  padding: 11px 13px;
+  background: #f9f9fa;
+  margin: 3px;
+  border-radius: 4px;
+  outline: 0;
+  border: 1px solid rgba(245, 245, 245, 0.7);
+  font-size: 14px;
+  transition: all 0.3s ease-out;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
+  :focus,
+  :hover {
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
+  }
+}
 `;
 
-const InfoLabel = styled.div`
+const Input = styled.input`
   display: block;
+  width: 300px;
+  padding: 11px 13px;
+  background: #f9f9fa;
+  color: #9dc3e6;
+  margin: 5px 3px;
+  border-radius: 4px;
+  outline: 0;
+  border: 1px solid rgba(245, 245, 245, 0.7);
+  font-size: 14px;
+  transition: all 0.3s ease-out;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
+  :focus,
+  :hover {
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
+  }
+}
+`;
+
+const Table = styled.table`
+  border-collapse: collapse;
   width: 100%;
 `;
 
-const InfoValue = styled.div`
-  font-weight: bold;
+const TableRow = styled.tr`
+  border-bottom: 1px solid #ddd;
+`;
+
+const TableHeader = styled.th`
+  padding: 0.5rem;
+  border-right: 1px solid #ddd;
+  background: #eef4f7;
+  vertical-align: middle;
+  font-size: 13px;
+  width: 30%;
+  text-align: center;
+`;
+
+const TableCell = styled.td`
+  padding: 0.5rem;
+  width: 70%;
 `;
 
 function MyPage() {
   const [userData, setUserData] = useState({
     profilePicture: "",
-    id: "",
-    email: "",
     nickname: "",
-    name: "",
-    gender: "",
   });
+  const [selectedProvince, setSelectedProvince] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [activeTab, setActiveTab] = useState("myinfo");
+
+  const handleProvinceChange = e => {
+    setSelectedProvince(e.target.value);
+    setSelectedCity("");
+  };
+
+  const handleCityChange = e => {
+    setSelectedCity(e.target.value);
+  };
+
+  const handleTabClick = tab => {
+    setActiveTab(tab);
+  };
 
   useEffect(() => {
     setUserData({
-      profilePicture: Logo,
-      id: "aaa",
-      email: "aaa@aaa.com",
+      profilePicture: Profile,
       nickname: "uw",
-      name: "언더워터",
-      gender: "여",
     });
   }, []);
 
   return (
     <>
-      <User />
-      <LogoBtn />
-      <Ocean>
-        <Wave></Wave>
-        <Wave></Wave>
-      </Ocean>
-      <Container>
-        <Title>마이페이지</Title>
-        <ProfilePicture src={userData.profilePicture} />
-        <Link to="/edit">
-          <Button>회원정보 변경</Button>
+      <Header>
+        <Link to="/">
+          <LogoIcon src={Logo2} />
         </Link>
-        <InfoContainer>
-          <InfoRow>
-            <InfoLabel>아이디</InfoLabel>
-            <InfoValue>{userData.id}</InfoValue>
-          </InfoRow>
-          <InfoRow>
-            <InfoLabel>이메일 주소</InfoLabel>
-            <InfoValue>{userData.email}</InfoValue>
-          </InfoRow>
-          <InfoRow>
-            <InfoLabel>닉네임</InfoLabel>
-            <InfoValue>{userData.nickname}</InfoValue>
-          </InfoRow>
-          <InfoRow>
-            <InfoLabel>이름</InfoLabel>
-            <InfoValue>{userData.name}</InfoValue>
-          </InfoRow>
-          <InfoRow>
-            <InfoLabel>성별</InfoLabel>
-            <InfoValue>{userData.gender}</InfoValue>
-          </InfoRow>
-        </InfoContainer>
+        <NavButton
+          selected={activeTab === "myinfo"}
+          onClick={() => setActiveTab("myinfo")}>
+          내 정보
+        </NavButton>
+        <NavButton
+          selected={activeTab === "likes"}
+          onClick={() => setActiveTab("likes")}>
+          좋아요 목록
+        </NavButton>
+        <NavButton
+          selected={activeTab === "reviews"}
+          onClick={() => setActiveTab("reviews")}>
+          후기 목록
+        </NavButton>
+        <NavButton
+          selected={activeTab === "withdrawal"}
+          onClick={() => setActiveTab("withdrawal")}>
+          회원 탈퇴
+        </NavButton>
+      </Header>
+      <Container>
+        {activeTab === "myinfo" && (
+          <div>
+            <Table>
+              <tbody>
+                <TableRow>
+                  <TableHeader>프로필 사진</TableHeader>
+                  <TableCell>
+                    <ProfilePicture src={userData.profilePicture} />
+                    <div>
+                      <EditButton>수정</EditButton>
+                      <DeleteButton>삭제</DeleteButton>
+                    </div>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHeader>닉네임</TableHeader>
+                  <TableCell>
+                    <Input placeholder={userData.nickname}></Input>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHeader>현재 거주 지역</TableHeader>
+                  <TableCell>
+                    <Select id="province" onChange={handleProvinceChange}>
+                      <option value="">도 선택</option>
+                      {Object.keys(cityOptions).map(province => (
+                        <option key={province} value={province}>
+                          {province}
+                        </option>
+                      ))}
+                    </Select>
+                    <Select id="city">
+                      <option value="">시/군/구 선택</option>
+                      {selectedProvince === ""
+                        ? Object.values(cityOptions)
+                            .flat()
+                            .map(city => (
+                              <option key={city} value={city}>
+                                {city}
+                              </option>
+                            ))
+                        : cityOptions[selectedProvince].map(city => (
+                            <option key={city} value={city}>
+                              {city}
+                            </option>
+                          ))}
+                    </Select>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHeader>비밀번호 변경</TableHeader>
+                  <TableCell>
+                    <Input placeholder="현재 비밀번호"></Input>
+                    <Input placeholder="새 비밀번호"></Input>
+                    <Input placeholder="새 비밀번호 확인"></Input>
+                  </TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </tbody>
+            </Table>
+          </div>
+        )}
+        {activeTab === "likes" && <div></div>}
+        {activeTab === "reviews" && <div>{/* 후기 목록 내용 */}</div>}
+        {activeTab === "withdrawal" && <div>{/* 회원 탈퇴 내용 */}</div>}
       </Container>
     </>
   );
