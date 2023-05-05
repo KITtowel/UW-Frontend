407e7fa716d9ba3f8803a83bb6e47562c467b275
HongGunWoo <foxrain.gg@gmail.com>
2023-05-05 21:09:49 +0900

    상세 가맹점 페이지 수정

--- src/pages/StoreMap/components/DetailStore.js
+++ src/pages/StoreMap/components/DetailStore.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
 import { HiOutlinePencilAlt } from 'react-icons/hi';
 import { AiOutlineAlert } from 'react-icons/ai';
+import { Pagination } from '../../../components';
 
 const Container = styled.div`
   position: fixed;
@@ -21,36 +22,33 @@ const Container = styled.div`
 const StoreHeader = styled.div`
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
   align-items: center;
   background-color: white;
   width: 100%;
   height: auto;
-  padding-top: 30px;
-  padding-bottom: 30px;
+  padding-top: 20px;
+  padding-bottom: 20px;
   border-radius: 3px;
 `;
 
 const StoreInfo = styled.div`
-  width: 100%;
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 7px;
+  justify-content: center;
 `;
 
 const Name = styled.h1`
   font-size: 1.6em;
   margin-right: 3px;
-  /* flex: 1; */
 `;
 
 const Rate = styled.h2`
   font-size: 0.95em;
+  margin-bottom: 15px;
 `;
 
 const Loc = styled.h2`
   font-size: 0.9em;
+  margin: 13px 0 5px 0;
 `;
 
 const Tag = styled.h3`
@@ -65,6 +63,7 @@ const LikeIcon = styled.div`
   width: 30px;
   height: 30px;
   color: #24A1E8;
+  margin-bottom: 5px;
 `;
 
 const LikeText = styled.h3`
@@ -74,7 +73,6 @@ const LikeText = styled.h3`
 const StoreMenu = styled(StoreHeader)`
   margin-top: 8px;
   margin-bottom: 8px;
-  height: auto;
   padding: 0;
 `;
 
@@ -126,7 +124,9 @@ const StoreReview = styled(StoreHeader)`
 `;
 
 const ReviewLists = styled.div`
-
+  overflow: auto;
+  border-bottom: 1.5px solid #aeaeae;
+  height: ${(props) => `${props.reviewHeight}px`};
 `;
 
 const ReviewItem = styled.div`
@@ -134,7 +134,8 @@ const ReviewItem = styled.div`
 `;
 
 const ReviewContent = styled.p`
-
+  padding: 3px 15px 10px 15px;
+  border-bottom: 1px solid #D9D9D9;
 `;
 
 const ReviewUserInfo = styled.div`
@@ -171,7 +172,6 @@ const ReviewReport = styled.div`
   cursor: pointer;
 `;
 
-
 const Title = styled.h2`
   align-self: flex-start;
   padding-left: 25px;
@@ -188,18 +188,35 @@ const Contour = styled.div`
 
 function DetailStore(props) {
   const [isLike, setIsLike] = useState(false);
+  const [page, setPage] = useState(1);
+  const [reviewHeight, setReviewHeight] = useState(100);
+
+  const headerRef = useRef();
+  const menuRef = useRef();
 
   const handleLikeBtn = () => {
     setIsLike((prev) => !prev)
   }
 
+  useEffect(() => {
+    menuRef.current && headerRef.current && setReviewHeight(window.innerHeight - menuRef.current.offsetHeight - headerRef.current.offsetHeight - 106);
+    const handleResize = () => {
+      menuRef.current && headerRef.current && setReviewHeight(window.innerHeight - menuRef.current.offsetHeight - headerRef.current.offsetHeight - 106);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <Container>
-      <StoreHeader>
-        <div style={{ display: "flex", justifyContent: "center" }}>
+      <StoreHeader ref={headerRef}>
+        <StoreInfo>
           <Name>GS옥계점</Name>
           <Tag>편의점</Tag>
-        </div>
+        </StoreInfo>
         <Loc>경북 구미시 옥계북로 39</Loc>
         <Rate>⭐️5.0 (리뷰 100개)</Rate>
         <LikeIcon isLike={isLike} onClick={handleLikeBtn}>
@@ -207,7 +224,7 @@ function DetailStore(props) {
         </LikeIcon>
         <LikeText>{isLike ? '내 맛집 목록에서 빼기' : '내 맛집 목록에 담기'}</LikeText>
       </StoreHeader>
-      <StoreMenu>
+      <StoreMenu ref={menuRef}>
         <Title>대표 메뉴</Title>
         <Contour />
         <Menu>
@@ -230,33 +247,36 @@ function DetailStore(props) {
         </Menu>
       </StoreMenu>
       <StoreReview>
-        <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between'}}>
+        <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between' }}>
           <Title>리뷰</Title>
-          <div style={{display: 'flex', alignItems: 'center', paddingRight: '15px', gap: '5px'}}>
-            <HiOutlinePencilAlt style={{color: '#24A1E8'}}/>
-            <div style={{fontSize: '0.8em', color: '#aeaeae'}}>리뷰 쓰기</div>
+          <div style={{ display: 'flex', alignItems: 'center', paddingRight: '15px', gap: '5px' }}>
+            <HiOutlinePencilAlt style={{ color: '#24A1E8' }} />
+            <div style={{ fontSize: '0.8em', color: '#aeaeae' }}>리뷰 쓰기</div>
           </div>
         </div>
         <Contour />
-        <ReviewLists>
-          <ReviewItem>
-            <ReviewUserInfo>
-              <ReviewProfile src='https://itcm.co.kr/files/attach/images/813/669/168/006/bd6bf95e10a24e98df6a7000339c36a9.png'/>
-              <div>
-                <ReviewID>pigeonTwo</ReviewID>
-                <ReviewDate>23/01/01</ReviewDate>
-              </div>
-              <ReviewReport>
-                <AiOutlineAlert style={{marginRight: '3px'}}/>
-                <div style={{fontSize: '0.8em'}}>리뷰 신고</div> 
-              </ReviewReport>
-              
-            </ReviewUserInfo>
-            <ReviewContent>
-              별빛 가온누리 포도 바나나 사과 가온누리 여우별 나래 옅구름 옅구름 도르레 별빛 사과 그루잠 아리아 나래 가온누리 다솜 소록소록 아슬라 아련 그루잠 달볓 안녕 다솜 늘품 다솜 감사합니다 소솜 바나나 사과 도담도담 아슬라 노트북 함초롱하다 나래 미리내 노트북 컴퓨터 나래 바람꽃 별빛 별하 별하 별빛 여우비 나비잠 그루잠 나래 감사합니다.
-            </ReviewContent>
-          </ReviewItem>
+        <ReviewLists reviewHeight={reviewHeight}>
+          {Array.from({ length: 15 }).map(() => (
+            <ReviewItem>
+              <ReviewUserInfo>
+                <ReviewProfile src='https://itcm.co.kr/files/attach/images/813/669/168/006/bd6bf95e10a24e98df6a7000339c36a9.png' />
+                <div>
+                  <ReviewID>pigeonTwo</ReviewID>
+                  <ReviewDate>23/01/01</ReviewDate>
+                </div>
+                <ReviewReport>
+                  <AiOutlineAlert style={{ marginRight: '3px' }} />
+                  <div style={{ fontSize: '0.8em' }}>리뷰 신고</div>
+                </ReviewReport>
+
+              </ReviewUserInfo>
+              <ReviewContent>
+                별빛 가온누리 포도 바나나 사과 가온누리 여우별 나래 옅구름 옅구름 도르레 별빛 사과 그루잠 아리아 나래 가온누리 다솜 소록소록 아슬라 아련 그루잠 달볓 안녕 다솜 늘품 다솜 감사합니다 소솜 바나나 사과 도담도담 아슬라 노트북 함초롱하다 나래 미리내 노트북 컴퓨터 나래 바람꽃 별빛 별하 별하 별빛 여우비 나비잠 그루잠 나래 감사합니다.
+              </ReviewContent>
+            </ReviewItem>
+          ))}
         </ReviewLists>
+        <Pagination total={30} limit={15} page={page} setPage={setPage} />
       </StoreReview>
     </Container>
   );
